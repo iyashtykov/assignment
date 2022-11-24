78635ea060a5ecc8508da7025776b800b0c587db
Igor Yashtykov <iyashtykov@codio.com>
2022-11-24 08:40:33 +0000

    rename duplicated name for java file avoid conflicts

--- codio_solution_example_files/Test.java
+++ codio_solution_example_files/Test.java
@@ -5,7 +5,7 @@
 
 import java.util.Random;
 
-public class test {
+public class Test {
 	public static void main (String[] args){
 		// Generate a random number between 1-100. (See generateRandomNumber method.)
 		int random = generateRandomNumber(100);
