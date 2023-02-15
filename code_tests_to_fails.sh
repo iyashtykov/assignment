12062dbe4fa71ad6918d72ba7f85f5da18c8d4b5
Igor Yashtykov <iyashtykov@codio.com>
2023-02-15 13:31:28 +0000

    move file before rename folder

--- code_tests_to_fails.sh
+++ code_tests_to_fails.sh
@@ -1,4 +1,4 @@
+mv /home/codio/workspace/code_tests/javafolder/JavaClassStyleOk1.java /home/codio/workspace/code_tests_fails/javafolder/JavaClassStyleOk1.java
 mv /home/codio/workspace/code_tests /home/codio/workspace/code_tests_passed
 mv /home/codio/workspace/code_tests_fails /home/codio/workspace/code_tests
-mv /home/codio/workspace/code_tests_passed/javafolder/JavaClassStyleOk1.java /home/codio/workspace/code_tests/javafolder/JavaClassStyleOk1.java
 echo "//comment" >> /home/codio/workspace/code_tests/javafolder/JavaClassStyleOk1.java
